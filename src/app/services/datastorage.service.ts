370baaa4e252976d595e2ca319108f15c2a0431c
Mateo-valencia <42659666+Mateo-valencia@users.noreply.github.com>
2019-11-19 09:46:39 -0500

    Finish Project Full Function

--- src/app/services/datastorage.service.ts
+++ src/app/services/datastorage.service.ts
@@ -7,28 +7,24 @@ import { Article } from '../../interfaces/interfaces';
 })
 export class DatastorageService {
 
-  public noticias: Article[] = [];
+  noticias: Article[] = [];
 
   constructor(private storage: Storage) {
     this.GetData();
   }
 
   SetData( noticia: Article) {
-
     const existe = this.noticias.find( noti => noti.title === noticia.title );
-
     if (!existe) {
       this.noticias.unshift( noticia );
-      this.storage.set( 'Favoritos', noticia );
+      this.storage.set( 'Favoritos', this.noticias );
     }
-
   }
 
   async GetData() {
-
     const favoritos = await this.storage.get('Favoritos');
 
-    if (favoritos) {
+    if ( favoritos ) {
       this.noticias = favoritos;
     }
 
